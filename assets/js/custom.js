7f06c876020cbc74641cdc974b8ea6d1e8bc2b63
Cesar H. Damascena <cesardamascena10@hotmail.com>
2018-06-26 01:41:07 -0300

    ajustes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,22 +24,54 @@ $(".inner-div .visto-input").on("keyup", function() {
 
 
 // PAS
-$(".pa .switch.on input[type='checkbox']").on('click', function() {
-	if($(this).is(":checked")) {
-		$(this).closest('.card-box').find('.overlay').css('display', 'block');
-		$(this).closest('.contact-card').css('display', 'none');
-		$(this).closest('.pa').find('.switch.off input[type="checkbox"]').removeAttr('checked');
-	}
-}); 
+// $(".pa .switch.on input[type='checkbox']").on('click', function() {
+// 	if($(this).is(":checked")) {
 
-$(".pa .switch.off input[type='checkbox']").on('click', function() {
-	if($(this).is(":checked")) {
-		$(this).closest('.card-box').find('.overlay').css('display', 'none');
-		$(this).closest('.card-box').find('.contact-card').css('display', 'block');
-		$(this).closest('.card-box').find('.switch').removeProp('checked');
-		$(this).closest('.pa').find('.switch.on input[type="checkbox"]').removeAttr('checked');
-	}
-}); 
+// 		$(this).closest('.card-box').find('.overlay').css('display', 'block');
+// 		$(this).closest('.contact-card').css('display', 'none');
+// 		$(this).closest('.pa').find('.switch.off input[type="checkbox"]').removeAttr('checked');
+// 		$(this).closest('.card-box').find('.overlay').css('background-color', 'white');
+// 		$(".switch input[type='checkbox']").attr('checked');
+// 	}
+// }); 
+
+// $(".pa .switch.off input[type='checkbox']").on('click', function() {
+// 	if($(this).is(":checked")) {
+// 		$(this).closest('.card-box').find('.overlay').css('display', 'none');
+// 		$(this).closest('.card-box').find('.contact-card').css('display', 'block');
+// 		// $(this).closest('.card-box').find('.switch').removeProp('checked');
+// 		$(this).closest('.pa').find('.switch.on input[type="checkbox"]').removeAttr('checked');
+// 		// console.log('lala');
+// 	}
+
+// }); 
+
+
+$("#nome-pa-switch").on('click', function() {
+	event.preventDefault();
+	/* Act on the event */
+	$(this).closest('.card-box').find('.overlay').css('display', 'block');
+	$(this).closest('.contact-card').css('display', 'none');
+	$(this).closest('.card-box').find('.overlay').css('background-color', 'white');
+	$("#inner-nome-pa-switch").prop('checked', true);
+	$(".card-box").css('border-radius', '50px');
+});	
+
+$("#inner-nome-pa-switch").on('click', function() {
+	event.preventDefault();
+	/* Act on the event */
+	$(this).closest('.card-box').find('.overlay').css('display', 'none');
+	$(this).closest('.card-box').find('.contact-card').css('display', 'block');
+	$("#nome-pa-switch").removeAttr('checked');
+	$(".card-box").css('border-radius', '20px');
+});
+
+$("#pa-search").on('click', function() {
+	event.preventDefault();
+	/* Act on the event */
+	$(this).closest('.card-box').find('.overlay').css('display', 'none');
+	$(this).closest('.chart').find("overlay").css('display', 'block');
+});
 
 
 })(jQuery); // Fully reference jQuery after this point.
